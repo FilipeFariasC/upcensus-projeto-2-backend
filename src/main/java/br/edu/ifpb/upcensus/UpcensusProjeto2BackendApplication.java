f6e7aa046c796eac85b17501551624cd9ef82582
Filipe Farias <filipe.chagas@klok.tech>
2023-03-18 19:05:25 -0300

    inicializacao do projeto

---
+++ src/main/java/br/edu/ifpb/upcensus/UpcensusProjeto2BackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.upcensus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcensusProjeto2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcensusProjeto2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/upcensus/UpcensusProjeto2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.upcensus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcensusProjeto2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
